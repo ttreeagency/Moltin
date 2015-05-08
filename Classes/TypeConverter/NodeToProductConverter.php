d512f522a53b9ffc42cd0e3dd05e41c9444746ee
Dominique Feyer <dfeyer@ttree.ch>
2015-05-08 11:34:01 +0200

    [TASK] Rename ProductConverter to NodeToProductConverter

--- Classes/TypeConverter/NodeToProductConverter.php
+++ Classes/TypeConverter/NodeToProductConverter.php
@@ -2,17 +2,25 @@
 namespace Ttree\Moltin\TypeConverter;
 
 use Ttree\Moltin\Domain\Model\Product;
+use Ttree\Moltin\Domain\Model\Sku;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Property\PropertyMapper;
 use TYPO3\Flow\Property\PropertyMappingConfigurationInterface;
 use TYPO3\Flow\Property\TypeConverter\AbstractTypeConverter;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 
 /**
- * This Processor updates the Product NodeType with the existing
- * Categories from the Moltin API
+ * This Processor to convert a Node to a Moltin Product Properties
+ *
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
  */
-class ProductConverter extends AbstractTypeConverter {
+class NodeToProductConverter extends AbstractTypeConverter {
+
+	/**
+	 * @Flow\Inject
+	 * @var PropertyMapper
+	 */
+	protected $propertyMapper;
 
 	/**
 	 * @var array
@@ -41,11 +49,13 @@ class ProductConverter extends AbstractTypeConverter {
 	 * {@inheritdoc}
 	 */
 	public function convertFrom($source, $targetType, array $convertedChildProperties = array(), PropertyMappingConfigurationInterface $configuration = NULL) {
+		/** @var Sku $sku */
+		$sku = $this->propertyMapper->convert($source, 'Ttree\Moltin\Domain\Model\Sku');
 		/** @var NodeInterface $source */
 		return new Product([
 			'title' => $source->getProperty('title'),
 			'slug' => $source->getIdentifier(),
-			'sku' => $source->getProperty('productSku'),
+			'sku' => (string)$sku,
 			'price' => $source->getProperty('productPrice'),
 			'sale_price' => $source->getProperty('productSalePrice'),
 			'status' => $source->getProperty('productStatus'),
